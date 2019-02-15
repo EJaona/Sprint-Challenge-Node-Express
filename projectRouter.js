d6357c77fb3201a140f8e3d66bc8850c2b5adf66
Enoka Jaona <Ejaona@gmail.com>
2019-02-15 12:50:09 -0600

    completed crud for projects

---
+++ projectRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const projectDb = require("./data/helpers/projectModel");
+const projectRouter = express.Router();
+
+projectRouter.get("/:id", async (req, res) => {
+  const action = await projectDb.get(req.params.id);
+  //   const resp = action.actions.map(act => act.project_id);
+  res.status(200).json({ success: true, action });
+});
+
+projectRouter.post("/", async (req, res) => {
+  const action = await projectDb.insert(req.body);
+  res.status(200).json({ success: true, action });
+});
+
+projectRouter.put("/:id", async (req, res) => {
+  const action = await projectDb.update(req.params.id, req.body);
+  res.status(200).json({ success: true, action });
+});
+
+projectRouter.delete("/:id", async (req, res) => {
+  const action = await projectDb.remove(req.params.id);
+  res.status(200).json({ success: true, action });
+});
+
+module.exports = projectRouter;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const server = express();
 const actionRouter = require("./actionRouter");
+const projectRouter = require("./projectRouter");
 
 server.use(express.json());
 server.use("/action", actionRouter);
+server.use("/project", projectRouter);
 
 module.exports = server;
