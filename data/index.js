354d4632c27c1fe856c341941ea094f9214a5388
Enoka Jaona <Ejaona@gmail.com>
2019-02-15 12:13:39 -0600

    crud completed for actions

--- data/index.js
+++
@@ -1,5 +0,0 @@
-const server = require("./server");
-
-server.listen(4000, _ => {
-  console.log("\n** server is running on http://localhost:4000 **\n");
-});
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const server = express();
+const actionRouter = require("./actionRouter");
+
 server.use(express.json());
+server.use("/action", actionRouter);
 
 module.exports = server;
